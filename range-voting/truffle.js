3f0e7825fd253cab245c0d4fb7e6fe83bd7d0a5a
Eric Thomas <eric.l.m.thomas@gmail.com>
2018-02-17 14:37:51 -0700

    Temporary truffle config

--- range-voting/truffle.js
+++ range-voting/truffle.js
@@ -3,7 +3,9 @@ module.exports = {
     development: {
       host: 'localhost',
       port: 8545,
-      network_id: '*'
+      network_id: '*',
+      gas: 4700000,
+      gasPrice: 40000000000
     }
   }
 }
